3e10972f8efeab1d9d20a7b520a58afc5e38da18
aleksis.datseris <aleksis.datseris@ontotext.com>
2026-03-26 16:06:30 +0200

    update notes

---
+++ Sem_05/Task1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+
+const unsigned MAX_SIZE = 1024;
+
+int main()
+{
+    std::ifstream file("Task1.cpp");
+
+    if (!file.is_open())
+    {
+        std::cout << "File can't be opened!" << std::endl;
+        return -1;
+    }
+
+    while (!file.eof())
+    {
+        char buff[MAX_SIZE];
+        file.getline(buff, MAX_SIZE);
+        std::cout << buff << std::endl;
+    }
+
+    file.close();
+}
